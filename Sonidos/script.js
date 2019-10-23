c047997febb4c95f7ab60c75b80f1db9732f71c2
Luis Alfonso Briceño <lbriceno@gimnasiovolta.edu.co>
2019-10-22 22:18:25 -0500

    first commit

---
+++ Sonidos/script.js
@@ -0,0 +1,31 @@
+// let t = 0;//time of the 
+
+// const crearAnimal= function(posX, posY, velX, velY){
+//     return {
+//         posX,
+//         posY,
+//         velX,
+//         velY,
+//         move:function(){
+
+//         }
+//         moveTo: function(x, y){
+//             this.posX = x;
+//             this.posT = y;
+//         }
+//     }
+// }
+
+var vaca = new Audio('cow.wav');
+var perro = new Audio('dogShort.wav');
+var gato = new Audio('meow2.wav')
+
+function sonidoVaca(){
+    vaca.play();
+}
+function sonidoPerro(){
+    perro.play();
+}
+function sonidoGato(){
+    gato.play();
+}
